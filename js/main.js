d88ddae23d242da881aed7190335530521e3e730
tvclemana <tvclemana@addu.edu.ph>
2024-04-26 17:06:19 +0800

    First commit

--- js/main.js
+++ js/main.js
@@ -13,8 +13,41 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
+// Register the service worker
+import { openDB } from 'idb';
+
+if ('serviceWorker' in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener('load', async () => {
+    // Try to register the service worker.
+    try {
+      // Capture the registration for later use, if needed
+      let reg;
+
+      // Use ES Module version of our Service Worker in development
+      if (import.meta.env?.DEV) {
+        reg = await navigator.serviceWorker.register('/service-worker.js', {
+          type: 'module',
+        });
+      } else {
+        // In production, use the normal service worker registration
+        reg = await navigator.serviceWorker.register('/service-worker.js');
+      }
+
+      console.log('Service worker registered! 😎', reg);
+    } catch (err) {
+      console.log('😥 Service worker registration failed: ', err);
+    }
+  });
+}
 
 window.addEventListener('DOMContentLoaded', async () => {
+  // Set up the database
+const db = await openDB('settings-store', 1, {
+  upgrade(db) {
+    db.createObjectStore('settings');
+  },
+});
   // Set up the editor
   const { Editor } = await import('./app/editor.js');
   const editor = new Editor(document.body);
@@ -24,7 +57,26 @@ window.addEventListener('DOMContentLoaded', async () => {
   new Menu(document.querySelector('.actions'), editor);
 
   // Set the initial state in the editor
+  // Save content to database on edit
+  // Set up install prompt
+const { Install } = await import('./lib/install.js');
+new Install(document.querySelector('#install'));
+editor.onUpdate(async (content) => {
+  await db.put('settings', content, 'content');
+});
   const defaultText = `# Welcome to PWA Edit!\n\nTo leave the editing area, press the \`esc\` key, then \`tab\` or \`shift+tab\`.`;
 
-  editor.setContent(defaultText);
+  editor.setContent((await db.get('settings', 'content')) || defaultText);
+  
 });
+
+// Set up night mode toggle
+const { NightMode } = await import('./app/night-mode.js');
+new NightMode(
+  document.querySelector('#mode'),
+  async (mode) => {
+    editor.setTheme(mode);
+    // Save the night mode setting when changed
+  },
+  // Retrieve the night mode setting on initialization
+);
--- service-worker.js
+++ service-worker.js
@@ -13,3 +13,28 @@ Copyright 2021 Google LLC
  See the License for the specific language governing permissions and
  limitations under the License.
  */
+ import { warmStrategyCache } from 'workbox-recipes';
+ import { CacheFirst } from 'workbox-strategies';
+ import { registerRoute } from 'workbox-routing';
+ import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+ import { ExpirationPlugin } from 'workbox-expiration';
+ 
+ // Set up page cache
+ const pageCache = new CacheFirst({
+   cacheName: 'page-cache',
+   plugins: [
+     new CacheableResponsePlugin({
+       statuses: [0, 200],
+     }),
+     new ExpirationPlugin({
+       maxAgeSeconds: 30 * 24 * 60 * 60,
+     }),
+   ],
+ });
+ 
+ warmStrategyCache({
+   urls: ['/index.html', '/'],
+   strategy: pageCache,
+ });
+ 
+ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
